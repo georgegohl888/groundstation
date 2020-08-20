2f2fe06618d2158247e95e8b161f35011c10b05d
george gohl <georgegohl888@gmail.com>
2020-08-20 14:49:31 +0100

    hello

---
+++ GroundStation/GroundStation/AttitudeIndicator.xaml.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroundStation
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        SerialPort SP = new SerialPort("COM67", 57600);
+
+        private float _speed = 00;
+        public float Speed
+        {
+            get { return _speed; }
+            set
+            {
+                if (value != _speed)
+                {
+                    _speed = value;
+                    Update();
+                }
+            }
+        }//Speed Value
+
+        private float _altitude = 000;
+        public float Altitude
+        {
+            get { return _altitude; }
+            set
+            {
+                if (value != _altitude)
+                {
+                    _altitude = value;
+                    Update();
+                }
+            }
+        } // Altitude Value
+
+        private float _pitch = 00;
+        public float Pitch
+        {
+            get { return _pitch; }
+            set
+            {
+                if (value != _pitch)
+                {
+                    _pitch = value;
+                    Update();
+                }
+            }
+        }// Pitch Value
+        private float _roll = 00;
+        public float Roll
+        {
+            get { return _roll; }
+            set
+            {
+                if (value != _roll)
+                {
+                    _roll = value;
+                    Update();
+                }
+            }
+        }//Roll value
+        private float _heading = 360;
+        public float Heading
+        {
+            get { return _heading; }
+            set
+            {
+                if (value != _heading)
+                {
+                    _heading = value;
+                    Update();
+                }
+            }
+        }//Roll value
+        private float _fd_P = 1500;
+        public float FD_P
+        {
+            get { return _fd_P; }
+            set
+            {
+                    _fd_P = value;
+                    Update();
+            }
+        } // flight director pitch commanded
+        private float _fd_R = 1500;
+        public float FD_R
+        {
+            get { return _fd_R; }
+            set
+            {
+                    _fd_R = value;
+                    Update();
+            }
+        } // flight director Roll commanded
+        private bool _fd_Enabled = true;
+        public bool FD_Enabled
+        {
+            get { return _fd_Enabled; }
+            set {
+                if(value != _fd_Enabled)
+                {
+                    _fd_Enabled = value;
+                    Update();
+                }
+            }
+        }// is the flight director enabled?
+
+        public UserControl1()
+        {
+            InitializeComponent();
+            Update();
+        }
+        public void Update()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                TranslateTransform TT = new TranslateTransform(0, Pitch * 7.2);
+                TranslateTransform HT = new TranslateTransform(-Heading * 6, 0);
+
+                RotateTransform RT = new RotateTransform(-Roll);
+                RT.CenterX = Scale.Width / 2;
+                RT.CenterY = Scale.Height / 2;
+
+                TransformGroup TG = new TransformGroup();
+
+                TG.Children.Add(TT);
+                TG.Children.Add(RT);
+        
+                Scale.RenderTransform = TG;
+
+                // the sky ground
+                TranslateTransform TT2 = new TranslateTransform(0, Pitch * 7.2);
+
+                RotateTransform RT2 = new RotateTransform(-Roll);
+                RT2.CenterX = Image1.Width / 2;
+                RT2.CenterY = Image1.Height / 2;
+
+                TransformGroup TG2 = new TransformGroup();
+
+                TG2.Children.Add(TT2);
+                TG2.Children.Add(RT2);
+
+                Image1.RenderTransform = TG2;
+
+                RollIND.RenderTransform = RT;
+
+                HeadingTape.RenderTransform = HT;
+
+
+                if(FD_Enabled == false)
+                {
+                    FDI_P.Opacity = 0;
+                    FDI_R.Opacity = 0;
+                }
+                else if (FD_Enabled)
+                {
+                    FDI_P.Opacity = 100;
+                    FDI_R.Opacity = 100;
+                    TranslateTransform FDHT = new TranslateTransform(0, ((FD_P - 1500) / 5) * -0.75);
+                    TranslateTransform FDVT = new TranslateTransform(((FD_R - 1500) /5) * -0.75, 0);
+                    FDI_P.RenderTransform = FDHT;
+                    FDI_R.RenderTransform = FDVT;
+                }
+
+                TranslateTransform ST = new TranslateTransform(0, Speed * 19.5);
+                SpeedTape.RenderTransform = ST;
+
+
+                if (Speed < 100)
+                {
+                    SpeedActual.Text = Speed.ToString("00");
+                }
+                else
+                {
+                    SpeedActual.Text = Speed.ToString("Mx");
+                }
+
+
+                if (-20 < Altitude && Altitude < 300)
+                {
+                    TranslateTransform AT = new TranslateTransform(0, Altitude * 20);
+                    AltitudeTape.RenderTransform = AT;
+                }
+
+                AltitudeActual.Text = Altitude.ToString("000");
+            });
+
+            
+        }
+        
+
+    }
+}
---
+++ GroundStation/GroundStation/MainWindow.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using System.Drawing;
+using System.IO.Ports;
+using Microsoft.Win32;
+using System.IO;
+using GMap.NET;
+using GMap.NET.WindowsPresentation;
+//using GMap.NET.WindowsPresentation.Markers;
+using GMap.NET.MapProviders;
+using System.ComponentModel;
+
+namespace GroundStation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+       
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));   
+        }
+
+        StreamWriter Writer;
+        long timeoffset; //what will be used as the difference between actual time and mission time
+        SerialPort SP = new SerialPort("COM67", 57600);//com port conection
+
+        private string _flightStatus = "Not Connected";
+        public string FlightStatus
+        {
+            get { return _flightStatus; }
+            set { _flightStatus = value; OnPropertyChanged("FlightStatus"); }
+        }
+        private float _battVolt;
+        public float BattVolt
+        {
+            get { return _battVolt; }
+            set { _battVolt = value; OnPropertyChanged("BattVolt"); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SP.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            this.DataContext = this;
+            string[] AvailiblePorts = SerialPort.GetPortNames();
+            PortsList.ItemsSource = AvailiblePorts;
+            string reg = @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Avalon.Graphics\DisableHWAcceleration";
+            Microsoft.Win32.Registry.SetValue(reg, "", 0);
+            //FlightStatus = "Not Connected";
+
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e) //We've recieve some new data
+        {
+
+            // get the data from the port
+            string data = SP.ReadLine();
+            //decode the incoming data
+            try
+            {
+                switch (data.Substring(0, 1))
+                {
+                    case "R":
+                        PFD.Roll = float.Parse(data.Substring(1)) / 10;
+                        break;
+                    case "P":
+                        PFD.Pitch = float.Parse(data.Substring(1)) / 10;
+                        break;
+                    case "H":
+                        PFD.Heading = float.Parse(data.Substring(1)) / 10;
+                        break;
+                    case "S":
+                        PFD.Speed = float.Parse(data.Substring(1));
+                        break;
+                    case "A":
+                        PFD.Altitude = float.Parse(data.Substring(1));
+                        break;
+                    case "r":
+                        PFD.FD_R = float.Parse(data.Substring(1));
+                        break;
+                    case "p":
+                        PFD.FD_P = float.Parse(data.Substring(1));
+                        break;
+                    case "M":
+                        if(int.Parse(data.Substring(1)) == 1) 
+                        { Dispatcher.Invoke(() => { StatusBox.Foreground = System.Windows.Media.Brushes.Black; FlightStatus = "Autolevel"; }); }
+                        else if (int.Parse(data.Substring(1)) == 0) { this.Dispatcher.Invoke(() => { StatusBox.Foreground = System.Windows.Media.Brushes.Black; FlightStatus = "ManualFlight"; }); }
+                        break;
+                    case "V":
+                        BattVolt = float.Parse(data.Substring(1));
+                        break;
+
+                }
+
+            }
+            catch { }
+            if (Writer != null)
+            {
+                Writer.WriteLine((DateTime.Now.Ticks - timeoffset).ToString() + "|" + data);
+            }
+        }
+
+        private void ConnectDisconnect_Click(object sender, RoutedEventArgs e) // connect button has been pressed
+        {
+            if (PortsList.SelectedValue != null && SP.IsOpen == false) // connect the port
+            {
+                SP.PortName = PortsList.SelectedValue.ToString();
+                SP.Open();
+                ConnectDisconnect.Content = "Disconnect";
+                DebugLog.Inlines.Add("Connected to " + SP.PortName + "\n");
+                SP.WriteLine("Hello");
+            }
+            else if (SP.IsOpen)//disconnect the port
+            {
+                SP.WriteLine("Bye");
+                SP.Close();
+                if (Writer != null)
+                {
+                    Writer.Close();
+                }
+                Writer = null;
+                ConnectDisconnect.Content = "Connect";
+                DebugLog.Inlines.Add("Diconnected " + SP.PortName + "\n");
+                FlightStatus = "Not connected";
+                StatusBox.Foreground = System.Windows.Media.Brushes.Red;
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) // setup log file
+        {
+            SaveFileDialog FileDialog = new SaveFileDialog();
+            FileDialog.Filter = "BEANS Flight Log File (.txt) | *.txt";
+            FileDialog.ShowDialog();
+            if (FileDialog.FileName != null)
+            {
+                Writer = new StreamWriter(FileDialog.FileName);
+                DebugLog.Inlines.Add("Created Log at " + FileDialog.FileName + "\n");
+                timeoffset = DateTime.Now.Ticks;
+
+            }
+        }
+
+        private void FD_Enable_CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            PFD.FD_Enabled = true;
+            
+        }
+
+        private void FD_Enable_CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PFD.FD_Enabled = false;
+        }
+
+        private void PFD_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+
+            // choose your provider here
+            mapView.MapProvider = GMap.NET.MapProviders.GoogleSatelliteMapProvider.Instance;
+            mapView.MinZoom = 2;
+            mapView.MaxZoom = 20;
+            // whole world zoom
+            mapView.Zoom = 2;
+            // lets the map use the mousewheel to zoom
+            mapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            // lets the user drag the map
+            mapView.CanDragMap = true;
+            // lets the user drag the map with the left mouse button
+            mapView.DragButton = MouseButton.Left;
+
+            mapView.Position = new GMap.NET.PointLatLng(53.7233995, -0.5737999);
+            mapView.Zoom = 17;
+
+
+
+            GMap.NET.WindowsPresentation.GMapMarker marker2 = new GMap.NET.WindowsPresentation.GMapMarker(new GMap.NET.PointLatLng(53.7233995, -0.5737999));
+
+            String fp = AppDomain.CurrentDomain.BaseDirectory;
+            fp = System.IO.Path.Combine(fp, @"..\..\Images\Aeroplane icon 1.png");
+
+            BitmapImage b = new BitmapImage(new System.Uri(fp));
+
+
+
+        marker2.Shape = new Image
+            {
+                Width = 50,
+                Height = 50,
+                Source = b
+            };
+
+               
+            marker2.Offset = new System.Windows.Point(-25, -25);
+            mapView.Markers.Add(marker2);
+
+            // new System.Drawing.Bitmap(@"F:\documents\BEANS 2\BEANS-DEV\GroundStation\GroundStation\GroundStation\Images\Aeroplane icon 1.png")
+        }
+
+    }
+    
+
+}
